c5bbf8630efc23a9e15a55f73dd809dca62782c5
zeenat19 <zeenat19@navgurukul.org>
2020-12-16 22:03:31 +0530

    do loop

---
+++ 2_table.js
@@ -0,0 +1,6 @@
+do{
+    var i=2;
+    var j=1;
+    console.log(i*j);
+    j++
+}while(j<=10);
---
+++ ques1.js
@@ -0,0 +1,8 @@
+// Iterate with JavaScript Do...While LoopsPassed
+var ourArray = [];
+var i = 0;
+do {
+  ourArray.push(i);
+  i++;
+} while (i < 5);
+console.log(ourArray);
